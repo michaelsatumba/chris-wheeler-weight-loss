a097e4042bfb7ae04a7372dcd1733e0a77e61ff6
michaelsatumba <mjsatumba@gmail.com>
2024-05-09 15:00:47 -0700

    hamburger to x

--- application.js
+++ application.js
@@ -1,21 +1,28 @@
 document.getElementById('menu-btn').addEventListener('click', function() {
     var items = document.getElementById('menu-items');
+    var menuButton = document.getElementById('menu-btn');
     if (items.style.display === 'block') {
         items.style.display = 'none';
+        menuButton.classList.remove('open');  // Remove the 'open' class to revert to hamburger icon
     } else {
         items.style.display = 'block';
+        menuButton.classList.add('open');  // Add the 'open' class to change to 'X' icon
     }
 });
 
 // Reset menu display on window resize
 window.addEventListener('resize', function() {
     var items = document.getElementById('menu-items');
+    var menuButton = document.getElementById('menu-btn');
     if (window.innerWidth > 768) {
-        items.style.display = 'flex'; // ensure it's visible and using flex layout
+        items.style.display = 'flex'; // Ensure it's visible and using flex layout
+        menuButton.classList.remove('open');  // Ensure it reverts to hamburger icon on larger screens
     } else {
-        items.style.display = 'none'; // ensure it's hidden on mobile when menu button is not clicked
+        items.style.display = 'none'; // Ensure it's hidden on mobile when menu button is not clicked
+        menuButton.classList.remove('open');  // Ensure it reverts to hamburger icon
     }
 });
+
 
 
 var slideIndex = 1;
